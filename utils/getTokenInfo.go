77a0ff4fcfc40ded9bde4c38f1d3b2934d266081
Fernando Aguilar <feraguilar6985@gmail.com>
2022-05-19 22:52:39 -0600

    feat(utils):add get information token fuc

---
+++ utils/getTokenInfo.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/snowball-devs/backend-utec-inscriptions/models"
+	"github.com/snowball-devs/backend-utec-inscriptions/server"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetTokenInformation(s server.Server, tokenString string) (primitive.ObjectID, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(s.Config().JWTSecret), nil
+	})
+
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+
+	claims, ok := token.Claims.(*models.AppClaims)
+	if !ok {
+		return primitive.NilObjectID, errors.New("Somithing failed to getting information")
+	}
+	return claims.UserId, nil
+
+}
